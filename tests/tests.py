dd8f2c16661c2e0f471e769e38fd26f00b8b60cd
Abhishek Shrivastava <x.abhishek.flyhigh@gmail.com>
2016-05-29 18:29:21 +0530

    Two test cases - isUp and login , working

---
+++ tests/tests.py
@@ -0,0 +1,97 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+
+class SegAnnTest(unittest.TestCase):
+
+    # Global Variables
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_isSegAnnUp(self):
+        """
+        Test#1
+        This test is for checking whether the page is loading or not
+        """
+        print "Test#1 isSegAnnUp ?"
+        driver = self.driver
+        driver.get("http://localhost:8080")
+        assert "SegAnnDB" in driver.title
+
+    def test_login(self):
+        """
+        Test#2
+        This test is for testing whether the login functionality is working
+        """
+        print "Test#2 Persona login test"
+
+        driver = self.driver
+        driver.get("http://localhost:8080")
+
+        # Call the login method to login the user
+        self.login(driver)
+
+        wait = WebDriverWait(driver, 60)
+        assert wait.until(
+            EC.element_to_be_clickable((By.ID, "signout"))).is_displayed()
+
+    # def test_upload(self):
+        # """
+        # This test is for checking if we can successfully upload a profile
+        # """
+        # driver = self.driver
+        # driver.get("http://localhost:8080/upload")
+
+    def login(self, driver):
+        """
+        This method is internal to testing framework
+        It is used to login the user.
+        Each test requires the user to be logged in already
+
+        Parameters:
+            driver - reference to driver being used
+        """
+        # this is the tricky part
+        # We have to get the right handle for the correct popup login window
+        main_window_handle = driver.current_window_handle
+
+        driver.find_element_by_id('signin').click()
+
+        signin_window_handle = None
+
+        # iterating through all the handles to get the popup, since we only hve
+        # one popup, making use of that
+        while not signin_window_handle:
+            for handle in driver.window_handles:
+                if handle != main_window_handle:
+                    signin_window_handle = handle
+                    break
+
+        # switch to the signin popup
+        driver.switch_to.window(signin_window_handle)
+
+        wait = WebDriverWait(driver, 60)
+
+        email_field = wait.until(
+            EC.element_to_be_clickable((By.ID, "authentication_email")))
+
+        # this is the example user from mockmyid
+        email_field.send_keys("helloworld@mockmyid.com")
+
+        # xpath id obtained using firebug for the next button on persona dialog
+        driver.find_element_by_xpath(
+            "/html/body/div/section[1]/form/div[2]/div[1]/div/div[2]/p[4]/button[1]").click()
+
+        # switch to the main window
+        driver.switch_to.window(main_window_handle)
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
